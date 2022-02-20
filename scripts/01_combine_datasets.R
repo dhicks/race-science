2a2ce469f73d6e3aed2a0a9503f1d6276c64fbf7
Dan Hicks <hicks.daniel.j@gmail.com>
2022-02-20 12:55:01 -0800

    fix paths in 01

--- scripts/01_combine_datasets.R
+++ scripts/01_combine_datasets.R
@@ -5,31 +5,35 @@ library(here)
 
 data_dir = here('data')
 
+write_csv = FALSE  ## Write phrases out to a single CSV? 
+
 ## Noun phrases ----
 # phrases_mq = open_dataset(here(data_dir, '00_phrases_mq'))
-phrases_mq = dataset_factory(here(data_dir, '00_phrases_mq'))
+# phrases_mq = dataset_factory(here(data_dir, '00_phrases_mq'))
 
 # phrases_ms = open_dataset(here(data_dir, '00_phrases_mainstr'))
-phrases_ms = dataset_factory(here(data_dir, '00_phrases_mainstr'))
+# phrases_ms = dataset_factory(here(data_dir, '00_phrases_mainstr'))
 
 ## This is very twitchy; manually combined parquet folders seems more stable
-phrases_comb = open_dataset(c(phrases_mq, phrases_ms))
-
+# phrases_comb = open_dataset(c(phrases_mq, phrases_ms))
 # write_dataset(phrases_comb, here(data_dir, '01_phrases'))
 
-phrases_comb |> 
-    head(nrow(phrases_comb)) |> 
-    write_csv_arrow(file = here(data_dir, '01_phrases.csv'))
+phrases_comb = open_dataset(here(data_dir, '00_phrases'))
 
+if (write_csv) {
+    phrases_comb |> 
+        compute() |>
+        write_csv_arrow(file = here(data_dir, '01_phrases.csv'))
+}
 
 ## Metadata ----
-meta_mq = open_dataset(here(data_dir, 'meta_mq.csv'), format = 'csv') |> 
+meta_mq = open_dataset(here(data_dir, '00_meta_mq.csv'), format = 'csv') |> 
     mutate(container.title = 'Mankind Quarterly') |> 
     select(article_id, container.title, year, title, volume, issue) |> 
     collect() |> 
     mutate(across(.cols = c(volume, issue), as.character))
 
-meta_ms = open_dataset(here(data_dir, 'meta_mainstr')) |> 
+meta_ms = open_dataset(here(data_dir, '00_meta_mainstr')) |> 
     rename(article_id = doi) |> 
     select(article_id, container.title, year, title, volume, issue) |> 
     collect()
